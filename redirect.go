abc3e0ebfaed010d159b6bea5985045beaf813fb
Pim van Gurp <PWD.vanGurp@student.han.nl>
2017-05-22 14:19:22 +0200

    :sparkles: Redirect urls to affiliate webshop Redirect.go has been added. A feature that redirects incomming short urls to their corresponding webshop urls. The feature has been added to shorten Webshop urls and make them more descriptive.

---
+++ redirect.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+const webshopMainUrl = "https://www.bookdepository.com?a_aid=Booktribe"
+
+var books map[string]string = make(map[string]string)
+
+func redirectUrl(res http.ResponseWriter, req *http.Request) {
+	url := findBookRedirect(req.URL.Path[1:])
+	http.Redirect(res, req, url, 302)
+}
+
+func findBookRedirect(filename string) string {
+	cacheRedirect, found := books[filename]
+	if found {
+		return cacheRedirect
+	}
+	fileRedirect, err := ioutil.ReadFile("./books/" + filename + ".txt")
+	if err != nil {
+		return webshopMainUrl
+	}
+	books[filename] = string(fileRedirect)
+	return books[filename]
+}
+
+func main() {
+	http.HandleFunc("/", redirectUrl)
+	http.ListenAndServe(":8080", nil)
+}
